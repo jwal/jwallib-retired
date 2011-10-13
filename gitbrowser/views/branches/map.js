96a954b6e62333e4c5722855a0c360e6e03a1cbb
James Ascroft-Leigh <jwal@jwal.me.uk>
2011-10-13 19:34:58 +0100

    Fix typo

--- gitbrowser/views/branches/map.js
+++ gitbrowser/views/branches/map.js
@@ -1,8 +1,8 @@
 function(doc) {
     if (doc.type == "git-branches") {
-	for (var i = 0; i < doc.branches.length, i++) {
+	for (var i = 0; i < doc.branches.length; i++) {
 	    var branch = doc.branches[i];
 	    emit(branch._id);
 	}
     }
-};
+}
