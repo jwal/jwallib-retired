8f9d46bac850d9a3c824501db2ae384b16b613a1
James Ascroft-Leigh <jwal@jwal.me.uk>
2011-12-08 21:59:19 +0000

    Debugging

--- gitbrowser/_attachments/app.js
+++ gitbrowser/_attachments/app.js
@@ -255,6 +255,7 @@ function process_hashchange()
     $("#main_body").html("<em>Loading...</em>");
     if (startswith(location.hash, "#show/"))
     {
+	throw new Error("fail");
 	console.debug("asked to show", trim_prefix(location.hash, "#show/"));
 	var path = split_path(trim_prefix(location.hash, "#show/"));
 	console.debug(path);
