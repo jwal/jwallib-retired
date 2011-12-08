244284f192b3de131ae6320cdfb3451b076ecc5a
James Ascroft-Leigh <jwal@jwal.me.uk>
2011-12-08 22:47:13 +0000

    Tree rendering

--- gitbrowser/_attachments/app.js
+++ gitbrowser/_attachments/app.js
@@ -158,25 +158,31 @@ function show_file_or_folder(app, branch_name, revision, path)
     }
     function render_tree_or_blob(doc)
     {
+	if (doc.type == "git-blob")
 	{
 	    if (doc.encoding == "raw")
 	    {
-	  	$("#main_body").html('<pre id="blob_data"></pre>');
-	  	$("#blob_data").text(doc.raw);
+		$("#main_body").html('<pre id="blob_data"></pre>');
+		$("#blob_data").text(doc.raw);
 	    }
 	    else if (doc.encoding == "base64")
 	    {
-	  	$("#main_body").html(
+		$("#main_body").html(
 	  	    '<p>File may be binary: '
 	  		+ '<span id="filepath"></span>'
 	  		+ '<pre id="blob_data"></pre>');
-	  	$("#filepath").text(path);
-	  	$("#blob_data").text(hexdump(bdoc.base64));
+		$("#filepath").text(path);
+		$("#blob_data").text(hexdump(bdoc.base64));
 	    }
 	    else
 	    {
-		throw new Error("Don't know how to render: " + doc);
+		throw new Error("Don't know how to render blob: "
+				+ doc.encoding);
 	    }
+	}
+	else
+	{
+	    throw new Error("Don't know how to render: " + doc.type);
 	}
     }
     function handle_tree_or_blob(doc, remaining_path)
