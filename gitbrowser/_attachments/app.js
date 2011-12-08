8f306ffa649e2229e938f841fa6c2e53bae829aa
James Ascroft-Leigh <jwal@jwal.me.uk>
2011-12-08 22:14:32 +0000

    Fix

--- gitbrowser/_attachments/app.js
+++ gitbrowser/_attachments/app.js
@@ -149,7 +149,7 @@ function split_path(path_string)
 	    throw new Error("Path component seems to contain an empty string"
 			    + ": " + path_string);
 	}
-	result.push(result)
+	result.push(decoded);
     }
     console.debug(result);
     return result;
