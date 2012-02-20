9f0dec46af9b4c6143f761bb278a9196213abdcd
James Ascroft-Leigh <jwal@jwal.me.uk>
2012-02-20 23:07:30 +0000

    Now with a test case, failing

---
+++ test_awalletextractor.py
@@ -0,0 +1,15 @@
+import awalletextractor
+import os
+import unittest
+
+class AwalletExtractorTest(unittest.TestCase):
+
+    def test(self):
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        input_path = os.path.join(base_path, "awalletextractor_example.crypt")
+        with open(input_path, "rb") as fh:
+            crypt_data = fh.read()
+        print awalletextractor.awallet_extract(crypt_data, "guessme")
+
+if __name__ == "__main__":
+    unittest.main()
