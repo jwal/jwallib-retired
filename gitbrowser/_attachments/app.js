6bf9592d6d7bbd95f5cdd1553a65bfc6276662ff
James Ascroft-Leigh <jwal@jwal.me.uk>
2012-10-05 01:06:08 +0100

    Beginnings of multiple file type support and client-side binary/text file detection

--- gitbrowser/_attachments/app.js
+++ gitbrowser/_attachments/app.js
@@ -2,7 +2,6 @@ var base_path = "/";
 var db_base = base_path + "db/";
 
 function b64decode(b64data) {
-    
     /* Nod to http://www.webtoolkit.info/javascript-base64.html */
     
     var keys = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -37,9 +36,8 @@ function b64decode(b64data) {
     return output;
 }
 
-function hexdump(b64data)
+function hexdump(charcodes)
 {
-    var charcodes = b64decode(b64data);
     var hex_result = [];
     var str_result = [];
     var result = [];
@@ -93,13 +91,6 @@ function startswith(string, prefix)
 {
     return (string.length >= prefix.length 
             && string.substring(0, prefix.length) == prefix);
-}
-
-function endswith(string, suffix)
-{
-    return (string.length >= suffix.length 
-            && string.substring(string.length - suffix.length, 
-				string.length) == suffix);
 }
 
 function trim_prefix(string, prefix)
@@ -190,6 +181,21 @@ function make_show_url(branch_name, revision, path)
     return "/" + (result.join("/"));
 }
 
+var EXTRA_TEXT_TYPES = [
+    "application/javascript",
+    "application/json",
+    "application/x-sh"
+];
+
+var HIGHLIGHT_MIME_TYPES = {
+    "text/x-python": {"name": "python", "line_func": function(l) {
+	return l.replace(/^#/gm, "");
+    }},
+    "application/javascript": {"name": "javascript", "line_func": function(l) {
+	return l.replace(/^\/\//gm, "");
+    }}
+};
+
 function show_file_or_folder(branch_name, revision, path)
 {
     if (revision != "head")
@@ -209,109 +215,126 @@ function show_file_or_folder(branch_name, revision, path)
 	}
 	if (doc.type == "git-blob")
 	{
-	    if (doc.encoding == "raw")
-	    {
+	    doc.basename = path[path.length - 1];
+	    guess_file_type(doc);
+	    var is_text = false;
+	    if (startswith(doc.mime_type, "text/")) {
+		is_text = true;
+	    } else {
+		for (var i = 0; i < EXTRA_TEXT_TYPES.length; i++) {
+		    if (doc.mime_type == EXTRA_TEXT_TYPES[i]) {
+			is_text = true;
+		    }
+		}
+	    }
+	    if (is_text) {
+		if (doc.encoding == "raw") {
+		    var text = doc.raw;
+		} else if (doc.encoding == "base64") {
+		    var text = utf8_decode(b64decode(doc.base64));
+		} else {
+		    throw new Error(doc.encoding);
+		}
 		var heading = $('<h1></h1>');
 		heading.text(path[path.length - 1]);
 		var converter = new Showdown.converter();
-		if (path[path.length - 1] == "README") {
-		    var div = $('<div>');
-		    div.html(converter.makeHtml(doc.raw));
-		    body.append(div);
-		} else {
-		    body.append(heading);
-		    var table = $('<table class="docco_table">'
-				  + '<col class="docs_column"></col>'
-				  + '<col class="code_column"></col>'
-				  + '</table>');
-		    var pre = $('<pre style="display: none"></pre>');
-		    if (endswith(path[path.length - 1], ".py")) {
-			pre.addClass("python");
+		body.append(heading);
+		var table = $('<table class="docco_table">'
+			      + '<col class="docs_column"></col>'
+			      + '<col class="code_column"></col>'
+			      + '</table>');
+		var pre = $('<pre style="display: none"></pre>');
+		var line_func = function(l) {return l};
+		for (var i in HIGHLIGHT_MIME_TYPES) {
+		    if (doc.mime_type == i) {
+			pre.addClass(HIGHLIGHT_MIME_TYPES[i].name);
+			line_func = HIGHLIGHT_MIME_TYPES[i].line_func;
+			break;
 		    }
-		    pre.text(doc.raw);
-		    body.append(pre);
-		    hljs.highlightBlock(pre[0], null, true);
-		    var language = pre.attr("class");
-		    var peek = function(container, offset) {
-			var child_nodes = container.childNodes;
-			return (child_nodes.length >= 2
-				&& child_nodes[0].nodeType 
-                                   == child_nodes[0].ELEMENT_NODE
-				&& child_nodes[0].nodeName == "SPAN"
-				&& $(child_nodes[0]).hasClass("comment")
-				&& child_nodes[1].nodeType
-                                   == child_nodes[1].TEXT_NODE
-				&& startswith(child_nodes[1].data, "\n"));
-		    };
-		    while (pre[0].childNodes.length > 0) {
-			var row = $('<tr></tr>');
-			table.append(row);
-			var docs_cell = $('<td class="docs_column_cell"></td>');
-			var docs_pre = $('<pre></pre>');
-			docs_cell.append(docs_pre);
-			row.append(docs_cell);
-			var code_cell = $('<td class="code_column_cell"></td>');
-			var code_pre = $('<pre></pre>');
-			row.append(code_cell);
-			code_cell.append(code_pre);
-			var child_nodes = pre[0].childNodes;
-			while (peek(pre[0])) {
-			    var comment = pre[0].childNodes[0];
-			    var newline = pre[0].childNodes[1];
-			    if (newline.data == "\n") {
-				pre[0].removeChild(newline);
-				docs_pre.append($(newline));
-			    } else {
-				newline.data = trim_prefix(newline.data, "\n");
-				var unused = $('<div></div>');
-				unused.text("\n");
-				var new_node = unused[0].childNodes[0];
-				unused[0].removeChild(new_node);
-				docs_pre.append($(new_node));
-			    }
-			    pre[0].removeChild(comment);
-			    docs_pre.append($(comment));
-			}
-			if (docs_pre[0].childNodes.length > 0
-			    && pre[0].childNodes.length > 0 
-			    && pre[0].childNodes[0].nodeType
-			       == pre[0].childNodes[0].TEXT_NODE
-			    && startswith(pre[0].childNodes[0].data, "\n")) {
-			    continue;
-			}
-			while (pre[0].childNodes.length > 0 && !peek(pre[0])) {
-			    while (pre[0].childNodes.length > 0) {
-				var to_move = pre[0].childNodes[0];
-				pre[0].removeChild(to_move);
-				code_pre.append($(to_move));
-				if (to_move.nodeType == to_move.TEXT_NODE
-				    && endswith(to_move.data, "\n") 
-				    && peek(pre[0])) {
-				    break;
-				}
-			    }
-			}
-		    }
-		    _.each($(".docs_column_cell", table), function(i) {
-			var raw_code = $(i).text();
-			if (language == "python") {
-			    raw_code = raw_code.replace(/^#/gm, "");
-			}
-			$(i).html(converter.makeHtml(raw_code));
-		    });
-                    body.append(table);
 		}
-	    }
-	    else if (doc.encoding == "base64")
-	    {
-		var pre = $('<pre></pre>');
-		pre.text(hexdump(doc.base64));
+		pre.text(text);
 		body.append(pre);
-	    }
-	    else
-	    {
-		throw new Error("Don't know how to render blob: "
-				+ doc.encoding);
+		hljs.highlightBlock(pre[0], null, true);
+		var language = pre.attr("class");
+		var peek = function(container, offset) {
+		    var child_nodes = container.childNodes;
+		    return (child_nodes.length >= 2
+			    && child_nodes[0].nodeType 
+                            == child_nodes[0].ELEMENT_NODE
+			    && child_nodes[0].nodeName == "SPAN"
+			    && $(child_nodes[0]).hasClass("comment")
+			    && child_nodes[1].nodeType
+                            == child_nodes[1].TEXT_NODE
+			    && startswith(child_nodes[1].data, "\n"));
+		};
+		while (pre[0].childNodes.length > 0) {
+		    var row = $('<tr></tr>');
+		    table.append(row);
+		    var docs_cell = $(
+			'<td class="docs_column_cell"></td>');
+		    var docs_pre = $('<pre></pre>');
+		    docs_cell.append(docs_pre);
+		    row.append(docs_cell);
+		    var code_cell = $(
+			'<td class="code_column_cell"></td>');
+		    var code_pre = $('<pre></pre>');
+		    row.append(code_cell);
+		    code_cell.append(code_pre);
+		    var child_nodes = pre[0].childNodes;
+		    while (peek(pre[0])) {
+			var comment = pre[0].childNodes[0];
+			var newline = pre[0].childNodes[1];
+			if (newline.data == "\n") {
+			    pre[0].removeChild(newline);
+			    docs_pre.append($(newline));
+			} else {
+			    newline.data = trim_prefix(newline.data, "\n");
+			    var unused = $('<div></div>');
+			    unused.text("\n");
+			    var new_node = unused[0].childNodes[0];
+			    unused[0].removeChild(new_node);
+			    docs_pre.append($(new_node));
+			}
+			pre[0].removeChild(comment);
+			docs_pre.append($(comment));
+		    }
+		    if (docs_pre[0].childNodes.length > 0
+			&& pre[0].childNodes.length > 0 
+			&& pre[0].childNodes[0].nodeType
+			== pre[0].childNodes[0].TEXT_NODE
+			&& startswith(pre[0].childNodes[0].data, "\n")) {
+			continue;
+		    }
+		    while (pre[0].childNodes.length > 0 && !peek(pre[0])) {
+			while (pre[0].childNodes.length > 0) {
+			    var to_move = pre[0].childNodes[0];
+			    pre[0].removeChild(to_move);
+			    code_pre.append($(to_move));
+			    if (to_move.nodeType == to_move.TEXT_NODE
+				&& endswith(to_move.data, "\n") 
+				&& peek(pre[0])) {
+				break;
+			    }
+			}
+		    }
+		}
+		_.each($(".docs_column_cell", table), function(i) {
+		    var raw_code = $(i).text();
+		    raw_code = line_func(raw_code);
+		    $(i).html(converter.makeHtml(raw_code));
+		});
+                body.append(table);
+	    } else {
+		if (doc.encoding == "raw") {
+		    var charcodes = utf8_encode(doc.raw);
+		} else if (doc.encoding == "base64") {
+		    var charcodes = b64decode(doc.base64);
+		} else {
+		    throw new Error(doc.encoding);
+		}
+		var pre = $('<pre></pre>');
+		pre.text(hexdump(charcodes));
+		body.append(pre);
 	    }
 	}
 	else if (doc.type == "git-tree")
@@ -398,6 +421,30 @@ function show_file_or_folder(branch_name, revision, path)
     }
     $.get(db_base + "git-branches",
 	  {}, handle_branches, "json");
+}
+
+function utf8_decode(byte_codes) {
+    var codes = [];
+    for (var i = 0; i < byte_codes.length; i++) {
+	var hex_string = "00" + byte_codes[i].toString(16);
+	var hex_string = hex_string.substring(hex_string.length - 2,
+					      hex_string.length);
+	codes.push("%" + hex_string);
+    }
+    return decodeURIComponent(codes.join(""));
+}
+
+function utf8_encode(string) {
+    var encoded = [];
+    for (var i = 0; i < string.length; i++) {
+	var strchr = encodeURIComponent(string[i]);
+	if (strchr.length == 1) {
+	    encoded.push(strchr.charCodeAt(0))
+	} else {
+	    encoded.push(parseInt(trim_prefix(strchr, "%"), 16));
+	}
+    }
+    return encoded;
 }
 
 function process_hashchange(path)
