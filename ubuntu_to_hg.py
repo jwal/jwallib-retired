6a7680b0d0322d409346c897ef47a16fc21fc25b
James Ascroft-Leigh <jwal@jwal.me.uk>
2013-07-07 08:59:03 +0100

    Fetch the Packages and Sources files

--- ubuntu_to_hg.py
+++ ubuntu_to_hg.py
@@ -8,6 +8,7 @@ import optparse
 import requests
 import sys
 from posixpath import join
+import posixpath
 import email
 import apt_pkg
 from cStringIO import StringIO
@@ -17,10 +18,12 @@ from jwalutil import read_file
 from jwalutil import write_file
 from jwalutil import read_lines
 from jwalutil import group_by
+from jwalutil import trim
 import os
 from pprint import pformat
 from process import call
 import contextlib
+import hashlib
 
 BASE_URL = "http://changelogs.ubuntu.com/"
 
@@ -38,7 +41,11 @@ def parse_control_file(data):
 
 def get(*args, **kwargs):
     result = requests.get(*args, **kwargs)
-    result.raise_for_status()
+    try:
+        result.raise_for_status()
+    except Exception, e:
+        print get, args, kwargs
+        raise
     return result
 
 @contextlib.contextmanager
@@ -72,7 +79,8 @@ def ubuntu_to_hg(hg_path):
         return call(["hg"] + argv, **kwargs)
 
     with with_ubuntu_keyring() as gpg:
-        meta_release_data = get(join(BASE_URL, "meta-release")).text
+        meta_release_data = get(
+            join(BASE_URL, "meta-release-development")).content
         meta_release = parse_control_file(meta_release_data)
         group_by(meta_release, lambda r: r["Dist"])
         if not os.path.exists(hg_path):
@@ -87,50 +95,96 @@ def ubuntu_to_hg(hg_path):
         if len(read_lines(hg(["status"]))) > 0:
             hg(["commit", "-m", "Update from upstream"])
         ok_branches = set()
+        seen_supported_non_lts = False
         for release in meta_release:
             branch = "ubuntu_%s" % (release["Dist"],)
-            try:
-                if branch not in branches:
-                    hg(["update", "--clean", "default"])
-                    hg(["branch", "--force", branch])
-                else:
-                    hg(["update", "--clean", branch])
-                try:
-                    hg(["merge", "default"])
-                except Exception:
-                    pass
-                release_path = os.path.join(hg_path, "Release")
-                old_sha1sums = {}
-                if os.path.exists(release_path):
-                    old_sha1sums = get_release_sha1sums(read_file(release_path))
-                release_data = get(release["Release-File"]).text
-                new_sha1sums = get_release_sha1sums(release_data)
-                write_file(release_path, release_data)
-                release_gpg_data = get(release["Release-File"] + ".gpg").text
-                release_gpg_path = os.path.join(hg_path, "Release.gpg")
-                write_file(release_gpg_path, release_gpg_data)
-                gpg(["--verify", release_gpg_path, release_path])
-                for relpath in old_sha1sums:
-                    if relpath in new_sha1sums:
-                        continue
-                    # Delete file
-                for relpath in new_sha1sums:
-                    if relpath in old_sha1sums:
-                        if new_sha1sums[relpath] == old_sha1sums[relpath]:
-                            continue
-                    # Fetch file and check sha1sum
-                hg(["addremove"])
-                if len(read_lines(hg(["status"]))) > 0:
-                    hg(["commit", "-m", "Update from upstream"])
-            except Exception, e:
-                print e
+            is_lts = "LTS" in release["Version"]
+            is_supported = release["Supported"] == "1"
+            if is_supported and not is_lts:
+                seen_supported_non_lts = True
+            is_development = not is_supported and seen_supported_non_lts
+            # print "  branch", branch
+            # print "    is_supported", is_supported
+            # print "    is_lts", is_lts
+            # print "    is_development", is_development
+            if not is_supported and not is_development:
                 continue
             ok_branches.add(branch)
+            if branch not in branches:
+                hg(["update", "--clean", "default"])
+                hg(["branch", "--force", branch])
+            else:
+                hg(["update", "--clean", branch])
+            try:
+                hg(["merge", "default"])
+            except Exception:
+                pass
+            release_path = os.path.join(hg_path, "Release")
+            old_sha1sums = {}
+            if os.path.exists(release_path):
+                old_sha1sums = get_release_sha1sums(read_file(release_path))
+            release_data = get(release["Release-File"]).content
+            new_sha1sums = get_release_sha1sums(release_data)
+            write_file(release_path, release_data)
+            release_gpg_data = get(release["Release-File"] + ".gpg").content
+            release_gpg_path = os.path.join(hg_path, "Release.gpg")
+            write_file(release_gpg_path, release_gpg_data)
+            gpg(["--verify", release_gpg_path, release_path])
+            for relpath in old_sha1sums:
+                if relpath in new_sha1sums:
+                    continue
+                file_path = os.path.join(hg_path, relpath)
+                call(["rm", "-rf", "--one-file-system", file_path])
+            for relpath in new_sha1sums:
+                if relpath in old_sha1sums:
+                    if new_sha1sums[relpath] == old_sha1sums[relpath]:
+                        continue
+                if (relpath.endswith(".gz") 
+                        and trim(relpath, suffix=".gz") in new_sha1sums):
+                    continue
+                if (relpath.endswith(".bz2") 
+                        and trim(relpath, suffix=".bz2") in new_sha1sums):
+                    continue
+                file_path = os.path.join(hg_path, relpath)
+                url = posixpath.join(
+                    posixpath.dirname(release["Release-File"]), relpath)
+                try:
+                    file_data = get(url).content
+                except Exception, e:
+                    try:
+                        file_data_bz2 = get(url + ".bz2").content
+                    except Exception, e:
+                        try:
+                            file_data_gz = get(url + ".gz").content
+                        except Exception, e:
+                            print "  Failed", relpath
+                            continue
+                        else:
+                            file_data = call(["gunzip"], 
+                                             stdin_data=file_data_bz2,
+                                             do_crlf_fix=False)
+                    else:
+                        file_data = call(["bzip2", "-d"], 
+                                         stdin_data=file_data_bz2,
+                                         do_crlf_fix=False)
+                sha1sum = hashlib.sha1(file_data).hexdigest()
+                if sha1sum != new_sha1sums[relpath]:
+                    raise Exception("sha1sum mismatch for %r: "
+                                    "got %s expecting %s"
+                                    % (url, sha1sum, new_sha1sums[relpath]))
+                if not os.path.exists(os.path.dirname(file_path)):
+                    os.makedirs(os.path.dirname(file_path))
+                with open(file_path, "wb") as fh:
+                    fh.write(file_data)
+            hg(["addremove"])
+            if len(read_lines(hg(["status"]))) > 0:
+                hg(["commit", "-m", "Update from upstream"])
         for branch in branches:
             if branch == "default" or branch in ok_branches:
                 continue
             hg(["update", "--clean", branch])
-            hg(["commit", "--close-branch", "-m", "Closing failed branch"])
+            hg(["commit", "--close-branch", 
+                "-m", "Closing unsupported release"])
 
 def main(argv):
     parser = optparse.OptionParser(__doc__)
